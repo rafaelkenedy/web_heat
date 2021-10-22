e3a213603903ca86b68985b805c2016903bfcb2f
rafaelkenedy <k.rafaelalves@gmail.com>
2021-10-22 03:41:07 -0300

    first commit

---
+++ src/components/LoginBox/index.tsx
@@ -0,0 +1,25 @@
+import { useContext, useEffect } from 'react'
+import { VscGithubInverted, VscMultipleWindows } from 'react-icons/vsc'
+import { AuthContext } from '../../context/auth'
+import { api } from '../../services/api'
+import styles from './styles.module.scss'
+
+
+
+export function LoginBox(){
+
+    const { signInUrl } = useContext(AuthContext)
+
+    
+
+
+    return(
+        <div className={styles.loginBoxWrapper}>
+            <strong>Entre e compartilhe sua mensagem</strong>
+            <a href={signInUrl} className={styles.signInWithGithub}>
+                <VscGithubInverted size='24'/>
+                Entrar com github
+            </a>
+        </div>
+    )
+}
